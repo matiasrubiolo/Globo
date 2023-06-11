c28983857a33aeabb79dc71979cf3cf4832ffd48
Rubiolo, Matias <maty_rubiolo@outlook.com>
2023-06-11 15:03:49 -0300

    agregando componentes y funcionalidades

--- main.js
+++ main.js
@@ -1,43 +1,21 @@
 const app = Vue.createApp({
     data() {
         return{
-            logo:"https://i.imgur.com/Qx8uR89.png",
+            logo:"./assets/img/logo.png",
             usuario:"",
             comentarios: [],
 
-            foto:"https://i.imgur.com/99qgQJy.jpeg",
-            nombre: "Jane Doe",
-            email: "jane_doe@email.com",
-            fechaNacimiento : "01/05/1996",
-            telefono: "918-936-9585",
-            ciudad: "Tokyo, Japan",
-            trabajo: "Lawn N",
-            botonSeguir : "Seguir",
-            fondoSeguir: "#3b82f6",
-
             sobremi: "¡Hola a todos! Soy Jane, y actualmente vivo en la vibrante y emocionante ciudad de Tokio,Japón.Me encanta capturar la esencia de la vida urbana a través de mi lente,explorando el constraste ente la arquitectura moderna y las tradiciones centenarias que conviven en esta metrópolis única.Desde rascacielos deslumbrantes y calles bulliciosas hasta templos serenos y jardines tranquilos, encuentro inspiracion en cada rincón de esta increíble ciudad.",
             
-            publicacion: "https://i.imgur.com/bq9db41.jpeg ",
+            publicacion: "./assets/img/publicacion.jpeg",
 
             botonLike: "Me gusta",
             fondoLike: "#f4f4f5",
             textoLike: "#3b82f6",
-            likes:201,
-
-            comentario:""
+            likes:201
         } 
     },
     methods:{
-        seguidor(){
-            if(this.botonSeguir === "Seguir"){
-                this.botonSeguir = "Dejar de seguir";
-                this.fondoSeguir = "#ff0000";
-            }else{
-                this.botonSeguir = "Seguir";
-                this.fondoSeguir = "#3b82f6"
-
-            }
-        },
         darLike(){
             if(this.botonLike === "Me gusta"){
                 this.botonLike = "Quitar Me gusta";
@@ -52,22 +30,11 @@ const app = Vue.createApp({
                 this.textoLike = "#3b82f6";
             }
         },
-        comentar(){
-            if(this.usuario === ""){
-                this.$refs.usuario.focus();
-            }else if(this.comentario === ""){
-                this.$refs.comentario.focus();
-            }else{
-                let comentario = {
-                    usuario : this.usuario,
-                    comentario : this.comentario
-                }
-
-                this.usuario = "";
-                this.comentario = "";
-            }
+        guardarComentario(comentario){
+            this.comentarios.push(comentario);
+        },
+        datasIncompletos(){
+            this.$refs.usuario.focus();
         }
     }
 });
-
-const mountedApp = app.mount('#app');
